575406c4327a96964078a115586cd2bfb98e537d
Fredrik Welander <welandfr@arcada.fi>
2022-04-11 15:58:33 +0300

    Lektion 5

--- api/hotel/index.php
+++ api/hotel/index.php
@@ -1,33 +1,68 @@
 <?php
 
-    require_once("../../../../../local/hotel_config.php");
+require_once("../../../../../local/hotel_config.php");
 
-    // LÄXA: Skapa pdo-connection till postgresql här!
-    $pdo = null;
+// LÄXA: Skapa pdo-connection till postgresql här!
+try {
+  $conn_string = "pgsql:host=".$db_conf['host'].";port=5432;dbname=".$db_conf['dbname'];
+  // pgsql:host=128.214.253.167;port=5432;dbname=woqkeycs
 
+  $pdo = new PDO($conn_string, 
+    $db_conf['user'], $db_conf['password'],
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+  //echo "Connection works!";
 
-    // Berätta år browsern att vi tänker skicka JSON-data
-    header("Content-Type: application/json");
+} catch (PDOException $e) {
+  die($e->getMessage());
+}
 
-    // Vi plockar ut variablerna från URLen och sparar i $request_vars
-    parse_str($_SERVER['QUERY_STRING'], $request_vars); // ==> ARRAY
+// Berätta år browsern att vi tänker skicka JSON-data
+header("Content-Type: application/json");
+// Vi plockar ut variablerna från URLen och sparar i $request_vars
+parse_str($_SERVER['QUERY_STRING'], $request_vars); // ==> ARRAY
 
-    // vi plockar ut data från request-bodyn 
-    $request_json = file_get_contents('php://input');
-    $request_body = json_decode($request_json); // ==> OBJEKT
-    // Vi kan casta (byta datatyp) från objekt till array så här: 
-    $request_body_arr = (array) $request_body;
+// vi plockar ut data från request-bodyn 
+$request_json = file_get_contents('php://input');
+$request_body = json_decode($request_json); // ==> OBJEKT
+// Vi kan casta (byta datatyp) från objekt till array så här: 
+$request_body_arr = (array) $request_body;
 
-    // Vår response först som en PHP-array
-    $response = [
-      "bookings" => [], 
-      "guests" => [],
-      "conf" => $hello
-    ];
+// Vår response först som en PHP-array
+$response = [
+  "bookings" => [], 
+  "guests" => [],
+  "conf" => $db_conf["hello"]
+];
 
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+  
+  // Hämta alla gäster
+  $stmt = $pdo->prepare("SELECT 
+    *
+    FROM hotel_guest
+    ORDER BY lastname
+  ");
+  $stmt->execute();
+  $response['guests'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Omvandla PHP-arrayen till JSON och skriv ut
-    echo json_encode($response);
+  // Hämta alla bokningar med namn!
+  $stmt = $pdo->prepare("SELECT  
+    b.*,
+    g.firstname,
+    g.lastname,
+    g.firstname || ' ' || g.lastname AS guestname
+    /* dubbel-pipe || konkatenerar teckensträngar i PostgreSQL */
+  FROM 
+    hotel_booking b
+  INNER JOIN hotel_guest g ON
+    b.guest_id = g.id");
+
+  $stmt->execute();
+  $response['bookings'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+// Omvandla PHP-arrayen till JSON och skriv ut
+echo json_encode($response);
 
 
 
---
+++ lektion-5-hotel-app/script.js
@@ -0,0 +1,39 @@
+console.log("works")
+
+const URL = "https://cgi.arcada.fi/~welandfr/demo/wdbcms22-exempel/api/hotel/";
+
+async function getHotel() {
+  const resp = await fetch(URL);
+  const data = await resp.json();
+
+  console.log(data.guests);
+
+  // New school-loop och string-konkatenering:
+  let guests_html = "";
+  // for-of loopen loopar igenom alla element i en array
+  for (guest of data.guests) {  
+    // backtick-syntax är praktiskt för konkatenering
+    guests_html += `
+      <option value="${guest.id}">
+        ${guest.firstname} ${guest.lastname}
+      </option>`;
+  }
+  
+  // Old school-loop och string-konkatenering:
+  guests_html = "";
+  for (let i = 0; i < data.guests.length; i++) {
+    guests_html += '<option value="' 
+      + data.guests[i].id + '">' 
+      + data.guests[i].firstname
+      + ' '
+      + data.guests[i].lastname
+      + '</option>';
+  }
+
+  document.querySelector("#guest").innerHTML = guests_html;
+}
+getHotel();
+
+
+
+ 
